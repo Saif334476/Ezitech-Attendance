884819011989c7868a895dd80e01021e4bf6169d
Saif Ur Rehman <saif334476@gmail.com>
2024-11-06 06:34:38 -0800

    updated Student's attendance list

--- lib/student_screeens/view_attendance.dart
+++ lib/student_screeens/view_attendance.dart
@@ -1,3 +1,4 @@
+import 'package:attendence_system/reusable_widgets.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,21 @@ class ViewAttendance extends StatefulWidget {
 }
 
 class _ViewAttendanceState extends State<ViewAttendance> {
+  String? _selectedItem;
+  List<String> options = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
   String uId = FirebaseAuth.instance.currentUser!.uid;
   List<Map<String, dynamic>> _documents = []; // List to store document data
 
@@ -24,9 +40,7 @@ class _ViewAttendanceState extends State<ViewAttendance> {
         .snapshots() // Listen for real-time updates
         .listen((snapshot) {
       setState(() {
-        _documents = snapshot.docs
-            .map((doc) => doc.data() as Map<String, dynamic>)
-            .toList();
+        _documents = snapshot.docs.map((doc) => doc.data()).toList();
       });
     });
   }
@@ -41,39 +55,77 @@ class _ViewAttendanceState extends State<ViewAttendance> {
           style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white),
         ),
       ),
-      body: _documents.isEmpty
-          ? const Center(
-              child:
-                  CircularProgressIndicator()) // Loading indicator while fetching
-          : Padding(
-              padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-              child: ListView.builder(
-                itemCount: _documents.length,
-                itemBuilder: (context, index) {
-                  var docData = _documents[index];
-                  return Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.lightGreen),
-                    child: ListTile(
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            docData['Date'] ?? 'No date',
-                            style: const TextStyle(fontWeight: FontWeight.w900),
-                          ),
-
-                          Text(docData['Status'] ?? 'No status',
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.w900)),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "Please Select Month",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 22,
               ),
             ),
+          ),
+          DropdownButton<String>(
+            value: _selectedItem, // The current selected item
+            hint: const Text("Select Month"), // Placeholder text
+            onChanged: (String? newValue) {
+              setState(() {
+                _selectedItem = newValue; // Update the selected item
+              });
+            },
+            items: options.map((String option) {
+              return DropdownMenuItem<String>(
+                value: option,
+                child: Text(option), // Display option text
+              );
+            }).toList(),
+          ),
+          _documents.isEmpty
+              ? const Center(
+                  child:
+                      CircularProgressIndicator()) // Loading indicator while fetching
+              : Padding(
+                  padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                  child: SizedBox(
+                    height: 650,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.separated(
+                      itemCount: _documents.length,
+                      itemBuilder: (context, index) {
+                        var docData = _documents[index];
+                        return Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.lightGreen),
+                          child: ListTile(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  docData['Date'] ?? 'No date',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w900),
+                                ),
+                                Text(docData['Status'] ?? 'No status',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w900)),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(
+                          height: 10,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
